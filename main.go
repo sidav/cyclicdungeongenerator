2f4653f98da2ce59b5257b7fe271fc4e2409d630
Unknown <sidav94@gmail.com>
2019-04-07 22:29:25 +0300

    Moves the visualizer to a separate file.

--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"CyclicDungeonGenerator/layout_generation"
-	cw "TCellConsoleWrapper"
-	"fmt"
 	rnd "github.com/sidav/golibrl/random"
+	cw "github.com/sidav/golibrl/console/tcell_console"
 )
 
 func main() {
@@ -12,100 +10,9 @@ func main() {
 
 	// layout_generation.Benchmark(-1, false, true)
 
+	// cw.Init_console("CDG", cw.TCellRenderer)
 	cw.Init_console()
 	defer cw.Close_console()
 
-	key := "none"
-	desiredPatternNum := -1
-
-	for key != "ESCAPE" {
-		cw.Clear_console()
-		pattNum := rnd.Random(layout_generation.GetTotalPatternsNumber())
-		if desiredPatternNum != -1 {
-			pattNum = desiredPatternNum
-		}
-		generatedMap, genRestarts := layout_generation.Generate(pattNum)
-
-		if generatedMap == nil {
-			cw.PutString(":(", 0, 0)
-			cw.PutString(fmt.Sprintf("Generation failed even after %d restarts, pattern #%d", genRestarts, pattNum), 0, 1)
-			cw.PutString("Press ENTER to generate again or ESCAPE to exit.", 0, 2)
-			cw.Flush_console()
-			for key != "ESCAPE" && key != "ENTER" {
-				key = cw.ReadKey()
-			}
-			continue
-		} else {
-			putMap(generatedMap)
-			putMiniMapAndPatternNumberAndNumberOfTries(generatedMap, pattNum, desiredPatternNum, genRestarts)
-		}
-		cw.Flush_console()
-keyread:
-		for {
-			key = cw.ReadKey()
-			switch key {
-			case "=":
-				if desiredPatternNum < layout_generation.GetTotalPatternsNumber()-1 {
-					desiredPatternNum++
-				}
-			break keyread
-			case "-":
-				if desiredPatternNum > -1 {
-					desiredPatternNum--
-				}
-			break keyread
-			case " ", "ESCAPE":
-				break keyread
-			}
-		}
-	}
-}
-
-func putCharArray(x, y int, c *[][]rune) {
-	for i := 0; i < len(*c); i++ {
-		for j := 0; j < len((*c)[0]); j++ {
-			setcolorForRune((*c)[i][j])
-			cw.PutChar((*c)[i][j], x+i, y+j)
-		}
-	}
-}
-
-func putMap(a *layout_generation.LayoutMap) {
-	putCharArray(0, 0, a.WholeMapToCharArray())
-}
-
-
-func putMiniMapAndPatternNumberAndNumberOfTries(a *layout_generation.LayoutMap, pattNum, desiredPNum, restarts int) {
-	sx, sy := a.GetSize()
-	for y := 0; y < sy; y++ {
-		for x := 0; x < sx; x++ {
-			chr := a.GetCharOfElementAtCoords(x, y)
-			setcolorForRune(chr)
-			cw.PutChar(chr, x+sx*5 + 2, y)
-		}
-	}
-	cw.SetFgColor(cw.BEIGE)
-	cw.PutString(fmt.Sprintf("PATTERN SELECTED: #%d  ", desiredPNum), sx*5+2, sy+2)
-	cw.PutString(fmt.Sprintf("PATTERN USED: #%d  ", pattNum), sx*5+2, sy+3)
-	cw.PutString(fmt.Sprintf("Generation finised with %d restarts", restarts), sx*5+2, sy+4)
-
-}
-
-func setcolorForRune(chr rune) {
-	switch chr {
-	case '1', '2', '3', '4', '5', '6':
-		cw.SetFgColor(cw.DARK_CYAN)
-	case '.':
-		cw.SetFgColor(cw.BEIGE)
-	case '+':
-		cw.SetFgColor(cw.DARK_MAGENTA)
-	case '#':
-		cw.SetFgColor(cw.DARK_GRAY)
-	case '%':
-		cw.SetFgColor(cw.RED)
-	case '=':
-		cw.SetFgColor(cw.DARK_BLUE)
-	default:
-		cw.SetFgColor(cw.DARK_GREEN)
-	}
+	doLayoutVisualization()
 }
