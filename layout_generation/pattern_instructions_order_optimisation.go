b789c3709a44d182b98b5b1a3db95afa9f3a6c69
sidav <sidav94@gmail.com>
2021-04-23 23:56:23 +0300

    Optimization routine improvement.

--- layout_generation/pattern_instructions_order_optimisation.go
+++ layout_generation/pattern_instructions_order_optimisation.go
@@ -5,19 +5,56 @@ func (p *pattern) optimizeStepsOrder() {
 	//for i := range p.instructions {
 	//	fmt.Println(p.instructions[i].instructionText)
 	//}
-	// first, move all "add node" to the beginning
-	for i := range p.instructions {
-		for j := i + 1; j < len(p.instructions); j++ {
-			if p.instructions[i].actionType == ACTION_PLACE_NODE_AT_EMPTY ||
-				p.instructions[i].actionType == ACTION_PLACE_NODE_AT_PATH ||
-				p.instructions[i].actionType == ACTION_PLACE_NODE_NEAR_PATH ||
-				p.instructions[i].actionType == ACTION_PLACE_OBSTACLE_AT_COORDS {
-				break 
+
+	// move all "add node" to the beginning
+	instructionMoved := true
+	for instructionMoved {
+		instructionMoved = false
+		instructionNeedsToMove := false
+	iterateInstructionsUp:
+		for i := range p.instructions {
+			types := []int{
+				ACTION_PLACE_NODE_AT_EMPTY,
+				ACTION_PLACE_NODE_AT_PATH,
+				ACTION_PLACE_NODE_NEAR_PATH,
+				ACTION_PLACE_OBSTACLE_AT_COORDS,
 			}
-			p.swapInstructionsAtIndices(i, j)
+			checkedType := p.instructions[i].actionType
+			for _, typeFromList := range types {
+				if checkedType == typeFromList {
+					if instructionNeedsToMove {
+						p.moveInstructionToBeginningOrToCode(i, types)
+						instructionMoved = true
+						break iterateInstructionsUp
+					}
+				}
+			}
 		}
 	}
-	// second, move each "add path" as up as possible
+	// move all non-creating instructions to end
+	instructionMoved = true
+	for instructionMoved {
+		instructionMoved = false
+	iterateInstructionsDown:
+		for i := range p.instructions {
+			types := []int{
+				ACTION_PLACE_NODE_AT_EMPTY,
+				ACTION_PLACE_NODE_AT_PATH,
+				ACTION_PLACE_NODE_NEAR_PATH,
+				ACTION_PLACE_OBSTACLE_AT_COORDS,
+			}
+			checkedType := p.instructions[i].actionType
+			for _, typeFromList := range types {
+				if checkedType == typeFromList {
+					break iterateInstructionsDown
+				}
+			}
+			p.moveInstructionToEnd(i)
+			instructionMoved = true
+			break
+		}
+	}
+	// move each "add path" as up as possible
 	for i := range p.instructions {
 		for j := i + 1; j < len(p.instructions); j++ {
 			areNodesPlaced := false
@@ -25,6 +62,24 @@ func (p *pattern) optimizeStepsOrder() {
 				areNodesPlaced = p.areNodesPlacedUntilStep(p.instructions[j].nameFrom, p.instructions[j].nameTo, i)
 				if areNodesPlaced {
 					p.moveInstructionUpFromTo(j, i)
+				}
+			}
+		}
+	}
+	// move each lock in order of lockId
+	swapped := true
+	for swapped {
+		swapped = false
+		for i := range p.instructions {
+			if p.instructions[i].actionType != ACTION_SET_NODE_CONNECTION_LOCKED_FROM_PATH {
+				continue
+			}
+			for j := i + 1; j < len(p.instructions); j++ {
+				if p.instructions[j].actionType == ACTION_SET_NODE_CONNECTION_LOCKED_FROM_PATH {
+					if p.instructions[i].lockNumber > p.instructions[j].lockNumber {
+						p.swapInstructionsAtIndices(i, j)
+						swapped = true
+					}
 				}
 			}
 		}
@@ -68,5 +123,23 @@ func (p *pattern) moveInstructionUpFromTo(from, to int) {
 	}
 	for x := from; x > to; x-- {
 		p.swapInstructionsAtIndices(x, x-1)
+	}
+}
+
+func (p *pattern) moveInstructionToBeginningOrToCode(from int, codes []int) {
+	for x := from; x > 0; x-- {
+		checkedType := p.instructions[x-1].actionType
+		for _, typeFromList := range codes {
+			if checkedType == typeFromList {
+				break
+			}
+		}
+		p.swapInstructionsAtIndices(x, x-1)
+	}
+}
+
+func (p *pattern) moveInstructionToEnd(from int) {
+	for x := from; x < len(p.instructions)-1; x++ {
+		p.swapInstructionsAtIndices(x, x+1)
 	}
 }
